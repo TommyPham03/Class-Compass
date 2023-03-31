44bde2576d51135bd42327452e48f2098a3d19df
ThangN22 <98120212+ThangN22@users.noreply.github.com>
2023-03-30 20:08:58 -0500

    Made a method for retrieving the name of a Course

--- Course.py
+++ Course.py
@@ -2,10 +2,13 @@
 class Course:
     # method one: constructor
     def __init__(self, name, code, taken):
+        self.name = name
         self.code = code
         self.taken = taken
-    # method two: define ...
-
-    # method three: define ...
+    # method two: retrieve name
+    def getName(self):
+        print(self.name)
+    # method three: retrieve class code
 
 Java_I = Course("Introduction to Java", "CS1321", True)
+Java_I.getName()
