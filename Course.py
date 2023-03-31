6475cd11cf4237e62d99429515b4b66ea4d0a0ad
ThangN22 <98120212+ThangN22@users.noreply.github.com>
2023-03-30 20:04:45 -0500

    Made a constructor method for Course

--- Course.py
+++ Course.py
@@ -1,1 +1,11 @@
 # make a class that defines a course and its contents
+class Course:
+    # method one: constructor
+    def __init__(self, name, code, taken):
+        self.code = code
+        self.taken = taken
+    # method two: define ...
+
+    # method three: define ...
+
+Java_I = Course("Introduction to Java", "CS1321", True)
