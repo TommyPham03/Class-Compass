a78538da63fb626416e852d892859aa90cbec31e
ThangN22 <98120212+ThangN22@users.noreply.github.com>
2023-04-04 16:57:14 -0500

    Create a print method that provides all relevant detail of a course

--- Course.py
+++ Course.py
@@ -7,14 +7,33 @@ class Course:
         self.taken = taken
     # method two: retrieve name
     def getName(self):
-        print(self.name)
+        return self.name
     # method three: retrieve class code
     def getCode(self):
-        print(self.code)
+        return self.code
     #method four: retrieve whether class has been taken or not
     def isTaken(self):
-        print(self.taken)
+        return self.taken
+    #method five: retrieve hours the class is worth using course code
+    def getHours(self):
+        code = self.getCode()
+        hour = int(code[-1])
+        return hour
+    #method eight: get subject
+    def getSubject(self):
+        return self.getCode()[:-4]
+    #method seven: print
+    def print(self):
+        print("Course Name: " + self.getName() + "\n"
+              + "Subject: " + self.getSubject() + "\n"
+              + "Course Code: " + self.getCode() + "\n"
+              + "Course Hours: " + str(self.getHours()) + " Hours" + "\n"
+              + "Taken: " + str(self.isTaken()) + "\n")
+
 Java_I = Course("Introduction to Java", "CS1321", True)
-Java_I.getName()
-Java_I.getCode()
-Java_I.isTaken()
+Java_I.print()
+
+Calc_I_0 = Course("Calculus & Analyt Geometry I", "MATH1823", True)
+Calc_I_0.print()
+
+
